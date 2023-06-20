6583c7730fb70369fcced0af3034c1b846117e55
samah <samahch6@gmail.com>
2023-06-20 22:02:28 +0300

    implemented debounce technique with the search input

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 let resultsContainer = document.getElementsByClassName("container")[0]
 
+
+// Get the search input element
+const searchInput = document.getElementById('searchInput');
+// Initialize a timer variable
+let timer;
+
+// Function to handle debounce and perform search
+function debounceSearch() {
+    // Clear the previous timer
+    clearTimeout(timer);
+    // Set a new timer
+    timer = setTimeout(function() {
+      // Perform the search request
+      const query = searchInput.value;
+      //console.log("test1");
+      generateResults(query);
+    }, 3000); // Adjust the delay (in milliseconds) as needed
+  }
+  // Add event listener to the search input
+  //searchInput.addEventListener('keyup', debounceSearch);
+
+
 const validateInput = (el) => {
     if(el.value === ""){
         resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
     }else{
-        generateResults(el.value, el)
+        // Add event listener to the search input
+        searchInput.addEventListener('keyup', debounceSearch);
     }
 }
 
-const generateResults = (searchValue, inputField) => {
+
+
+const generateResults = (searchValue) => {
     fetch(
         "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
         + searchValue
@@ -18,7 +43,7 @@ const generateResults = (searchValue, inputField) => {
         let results = data.query.search
         let numberOfResults = data.query.search.length
         resultsContainer.innerHTML = ""
-        for(let i=0; i<numberOfResults; i++) {
+        for(let i=0; i < numberOfResults; i++) {
             let result = document.createElement("div")
             result.classList.add("results")
             result.innerHTML = `
@@ -30,8 +55,9 @@ const generateResults = (searchValue, inputField) => {
             `
             resultsContainer.appendChild(result)
         }
-        if(inputField.value === ""){
+        if(searchInput.value === ""){
             resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
         }
     })
+
 }
